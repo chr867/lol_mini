40c9441f6c64d6198a043e796fdfb6886cdc8a2b
chr867 <124451478+chr867@users.noreply.github.com>
2023-03-20 13:52:17 +0900

    배포

--- auto_insert_PJ.py
+++ auto_insert_PJ.py
@@ -6,39 +6,26 @@ import random
 
 
 def get_rawdata(tier_p):
-    match tier_p:
-        case 'C':
-            print(tier_p)
-            url_p = f'https://kr.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key={mu.riot_api_key}'
-            res_p = requests.get(url_p).json()
-            lst = random.sample(res_p['entries'], 5)
-            name_lst = [i['summonerName'] for i in lst]
+    if tier_p == 'C':
+        print(tier_p)
+        url_p = f'https://kr.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key={mu.riot_api_key}'
+        res_p = requests.get(url_p).json()
+        lst = random.sample(res_p['entries'], 5)
+        name_lst = [i['summonerName'] for i in lst]
 
-        case 'GM':
-            print(tier_p)
-            url_p = f'https://kr.api.riotgames.com/lol/league/v4/grandmasterleagues/by-queue/RANKED_SOLO_5x5?api_key={mu.riot_api_key}'
-            res_p = requests.get(url_p).json()
-            lst = random.sample(res_p['entries'], 5)
-            name_lst = [i['summonerName'] for i in lst]
+    elif tier_p == 'GM':
+        print(tier_p)
+        url_p = f'https://kr.api.riotgames.com/lol/league/v4/grandmasterleagues/by-queue/RANKED_SOLO_5x5?api_key={mu.riot_api_key}'
+        res_p = requests.get(url_p).json()
+        lst = random.sample(res_p['entries'], 5)
+        name_lst = [i['summonerName'] for i in lst]
 
-        case 'MASTER':
-            print(tier_p)
-            url_p = f'https://kr.api.riotgames.com/lol/league/v4/masterleagues/by-queue/RANKED_SOLO_5x5?api_key={mu.riot_api_key}'
-            res_p = requests.get(url_p).json()
-            lst = random.sample(res_p['entries'], 5)
-            name_lst = [i['summonerName'] for i in lst]
-
-        case _:
-            print(tier_p)
-            division_list_p = ['I', 'II', 'III', 'IV']
-            lst_p = []
-            for division_p in division_list_p:
-                page_p = random.randrange(1, 10)
-                url_p = f'https://kr.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/{tier_p}/{division_p}?page={page_p}&api_key={mu.riot_api_key}'
-                res_p = requests.get(url_p).json()
-                lst_p += random.sample(res_p, 5)
-            name_lst = [i['summonerName'] for i in lst_p]
-            # name_lst = list(map(lambda x: x['summonerName'], lst_p))
+    else:
+        print(tier_p)
+        url_p = f'https://kr.api.riotgames.com/lol/league/v4/masterleagues/by-queue/RANKED_SOLO_5x5?api_key={mu.riot_api_key}'
+        res_p = requests.get(url_p).json()
+        lst = random.sample(res_p['entries'], 5)
+        name_lst = [i['summonerName'] for i in lst]
 
     result_res = mu.match_timeline(name_lst, 3)
     result_df = pd.DataFrame(result_res, columns=['match_id', 'matches', 'timeline'])
@@ -51,7 +38,8 @@ def get_match_timeline_df(df_p):
     columns = [
         'match_id', 'gameDuration', 'gameVersion', 'summonerName', 'summonerLevel', 'participantId', 'championName',
         'champExperience', 'teamPosition', 'teamId', 'win', 'kills', 'deaths', 'assists', 'totalDamageDealtToChampions',
-        'totalDamageTaken', 'g_5', 'g_6', 'g_7', 'g_8', 'g_9', 'g_10', 'g_11', 'g_12', 'g_13', 'g_14', 'g_15', 'g_16',
+        'totalDamageTaken', 'wardsPlaced', 'wardsKilled', 'profileIcon', 'firstChampion', 'firstDragon', 'firstTower',
+        'g_5', 'g_6', 'g_7', 'g_8', 'g_9', 'g_10', 'g_11', 'g_12', 'g_13', 'g_14', 'g_15', 'g_16',
         'g_17', 'g_18', 'g_19', 'g_20', 'g_21', 'g_22', 'g_23', 'g_24', 'g_25'
         ]
     for m_idx, m in tqdm(enumerate(df_p['matches'])):
@@ -74,8 +62,23 @@ def get_match_timeline_df(df_p):
                 p['deaths'],
                 p['assists'],
                 p['totalDamageDealtToChampions'],
-                p['totalDamageTaken']
+                p['totalDamageTaken'],
+                p['wardsPlaced'],
+                p['wardsKilled'],
+                p['profileIcon']
             ])
+            if p['teamId'] == 100:
+                df_creater[-1].extend([
+                    m['info']['teams'][0]['objectives']['champion']['first'],
+                    m['info']['teams'][0]['objectives']['dragon']['first'],
+                    m['info']['teams'][0]['objectives']['tower']['first']
+                ])
+            else:
+                df_creater[-1].extend([
+                    m['info']['teams'][1]['objectives']['champion']['first'],
+                    m['info']['teams'][1]['objectives']['dragon']['first'],
+                    m['info']['teams'][1]['objectives']['tower']['first']
+                ])
             for t in range(5, 26):
                 try:
                     p_id = str(p['participantId'])
@@ -89,23 +92,28 @@ def get_match_timeline_df(df_p):
 
 def insert(t, conn):
     sql_insert = (
-        f'insert into LOL_MATCHES_TIER (match_id, gameDuration, gameVersion, summonerName, summonerLevel, '
+        f'insert into lol_mini (gameId, gameDuration, gameVersion, summonerName, summonerLevel, '
         f'participantId, championName, champExperience, teamPosition, teamId, win, kills, deaths,'
-        f'assists, totalDamageDealtTochampions, totalDamageTaken, g_5, g_6, g_7, g_8, g_9, g_10,'
+        f'assists, totalDamageDealtTochampions, totalDamageTaken, wardsPlaced, wardsKilled, profileIcon, firstChampion,'
+        f' firstDragon, firstTower, '
+        f'g_5, g_6, g_7, g_8, g_9, g_10,'
         f'g_11, g_12, g_13, g_14, g_15, g_16, g_17, g_18, g_19, g_20, g_21, g_22, g_23, g_24, g_25) '
         f'VALUES({repr(t.match_id)}, {t.gameDuration}, {repr(str(t.gameVersion))}, '
         f'{repr(t.summonerName)}, {t.summonerLevel}, {t.participantId}, {repr(t.championName)}, '
         f'{t.champExperience}, {repr(t.teamPosition)}, {t.teamId}, {repr(str(t.win))}, {t.kills}, '
-        f'{t.deaths}, {t.assists}, {t.totalDamageDealtToChampions}, {t.totalDamageTaken}, '
+        f'{t.deaths}, {t.assists}, {t.totalDamageDealtToChampions}, {t.totalDamageTaken}, {t.wardsPlaced}, {t.wardsKilled}, '
+        f'{t.profileIcon}, {repr(str(t.firstChampion))}, {repr(str(t.firstDragon))}, {repr(str(t.firstTower))},'
         f'{t.g_5}, {t.g_6}, {t.g_7}, {t.g_8}, {t.g_9}, {t.g_10}, '
         f'{t.g_11}, {t.g_12}, {t.g_13}, {t.g_14}, {t.g_15}, {t.g_16}, {t.g_17}, {t.g_18}, {t.g_19}, {t.g_20}, {t.g_21},'
         f'{t.g_22}, {t.g_23}, {t.g_24}, {t.g_25}) '
         f'ON DUPLICATE KEY UPDATE '
-        f'match_id = {repr(t.match_id)}, gameDuration = {t.gameDuration}, gameVersion = {repr(str(t.gameVersion))}, '
+        f'gameId = {repr(t.match_id)}, gameDuration = {t.gameDuration}, gameVersion = {repr(str(t.gameVersion))}, '
         f'summonerName = {repr(t.summonerName)}, summonerLevel = {t.summonerLevel}, participantId = {t.participantId}, '
         f'championName = {repr(t.championName)}, champExperience = {t.champExperience}, teamPosition = {repr(t.teamPosition)}, '
         f'teamId = {repr(t.teamId)}, win = {repr(str(t.win))}, kills = {t.kills}, deaths = {t.deaths}, assists = {t.assists}, '
-        f'totalDamageDealtToChampions = {t.totalDamageDealtToChampions}, totalDamageTaken = {t.totalDamageTaken}, '
+        f'totalDamageDealtToChampions = {t.totalDamageDealtToChampions}, totalDamageTaken = {t.totalDamageTaken},'
+        f'wardsPlaced = {t.wardsPlaced}, wardsKilled = {t.wardsKilled}, profileIcon = {t.profileIcon}, '
+        f'firstChampion = {repr(str(t.firstChampion))}, firstDragon = {repr(str(t.firstDragon))}, firstTower = {repr(str(t.firstTower))}, '
         f'g_5 = {t.g_5}, g_6 = {t.g_6}, g_7 = {t.g_7}, g_8 = {t.g_8}, g_9 = {t.g_9}, g_10 = {t.g_10}, g_11 = {t.g_11}, '
         f'g_12 = {t.g_12}, g_13 = {t.g_13}, g_14 = {t.g_14}, g_15 = {t.g_15}, g_16 = {t.g_16}, g_17 = {t.g_17}, g_18 = {t.g_18}, '
         f'g_19 = {t.g_19}, g_20 = {t.g_20}, g_21 = {t.g_21}, g_22 = {t.g_22}, g_23 = {t.g_23}, g_24 = {t.g_24}, g_25 = {t.g_25} '
@@ -123,7 +131,7 @@ def auto_insert(num=int):
     """
     tqdm.pandas()
     for count in tqdm(range(num)):
-        tier = ['IRON', 'BRONZE', 'SILVER', 'GOLD', 'PLATINUM', 'DIAMOND', 'MASTER', 'GM', 'C']
+        tier = ['MASTER', 'GM', 'C']
         idx = random.randrange(len(tier))
         rawdata_df = get_rawdata(tier[idx])
         result_df = get_match_timeline_df(rawdata_df)
--- python_miniPJ.py
+++ python_miniPJ.py
@@ -1,14 +1,40 @@
 import my_utils_PJ as mu
+import auto_insert_PJ as ai
+from tqdm import tqdm
+import imp
+imp.reload(ai)
+
+raw_df = ai.get_rawdata('MASTER')
+mt_df = ai.get_match_timeline_df(raw_df)
 
 sql_conn = mu.connect_mysql('lol_icia')
 sql_create = '''
 CREATE TABLE 
-LOL_MINI ( match_id varchar(20), gameDuration int, gameVersion varchar(20), summonerName varchar(20),
+LOL_MINI ( gameId varchar(20), gameDuration int, gameVersion varchar(20), summonerName varchar(20),
 summonerLevel int, participantId int, championName varchar(20), champExperience int, teamPosition varchar(10), 
 teamId int, win varchar(10), kills int, deaths int, assists int, totalDamageDealtToChampions int, totalDamageTaken int, 
+wardsPlaced int, wardsKilled int, profileIcon int, firstChampion varchar(10), firstDragon varchar(10), firstTower varchar(10), 
 g_5 int, g_6 int, g_7 int, g_8 int, g_9 int, g_10 int, g_11 int, g_12 int, g_13 int, g_14 int, g_15 int, g_16 int,
 g_17 int, g_18 int, g_19 int, g_20 int, g_21 int, g_22 int, g_23 int, g_24 int, g_25 int,
-CONSTRAINT LMT_PK_ID_PID PRIMARY KEY (match_id, participantId))
+CONSTRAINT LMT_PK_ID_PID PRIMARY KEY (gameId, participantId))
 '''
 mu.mysql_execute(sql_create, sql_conn)
 sql_conn.close()
+
+sql_conn = mu.connect_mysql('lol_icia')
+tqdm.pandas()
+mt_df.progress_apply(lambda x: ai.insert(x, sql_conn), axis=1)
+sql_conn.commit()
+sql_conn.close()
+
+raw_df['matches'][0]['info']['participants'][0]['teamId']
+raw_df['matches'][0]['info']['teams'][0]['teamId']
+raw_df['matches'][0]['info']['teams'][0]['objectives']['champion']['first']
+raw_df['matches'][0]['info']['teams'][0]['objectives']['dragon']['first']
+raw_df['matches'][0]['info']['teams'][0]['objectives']['tower']['first']
+raw_df['matches'][8]['info']['teams'][1]['teamId']
+raw_df['matches'][0]['info']['teams'][1]['objectives']['champion']['first']
+raw_df['matches'][0]['info']['teams'][1]['objectives']['dragon']['f']
+raw_df['matches'][0]['info']['teams'][1]['objectives']['tower']
+
+
