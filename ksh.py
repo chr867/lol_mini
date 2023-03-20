0441e2d423a5d434d0be83b1a81a824b8b4c6ede
chr867 <124451478+chr867@users.noreply.github.com>
2023-03-20 16:18:35 +0900

    최형로_와드 산점도

--- ksh.py
+++ ksh.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 import my_utils_PJ as mu
 from matplotlib import font_manager, rc
 font_path = "C:/Windows/Fonts/gulim.ttc"
-font = font_manager.FontProperties(fname = font_path).get_name()
+font = font_manager.FontProperties(fname=font_path).get_name()
 rc('font', family = font)
 
-sql_conn = mu.connect_mysql('lol')
+sql_conn = mu.connect_mysql('icia')
 df = mu.mysql_execute_dict('select * from lol_mini', sql_conn)
 df = pd.DataFrame(df)
 
@@ -18,8 +18,13 @@ sort_lose_df = tmp_df[tmp_df['win'] == 'False']
 sort_lose_df['win'].count()
 
 plt.figure()
-plt.plot(xdata = tmp_df['wardsPlaced'], ydata = sort_win_df['wardsKilled'], color = 'b', marker = 'o', linestyle = 'None')
-plt.plot(xdata = tmp_df['wardsPlaced'], ydata = sort_lose_df['wardsKilled'], color = 'r', marker = 'x', linestyle = 'None')
+xdata = sort_win_df['wardsPlaced']
+ydata = sort_win_df['wardsKilled']
+plt.plot(xdata, ydata, color='b', marker='o', linestyle='None')
+
+xdata2 = sort_lose_df['wardsPlaced']
+ydata2 = sort_lose_df['wardsKilled']
+plt.plot(xdata2, ydata2, color='r', marker='x', linestyle='None')
 plt.xlabel('와드 설치')
 plt.ylabel('와드 파괴')
 plt.grid(True)
